6d7ed64783b0e1ecdaa31a34a368489db23cf47e
bhanu11pratap <110561155+bhanu11pratap@users.noreply.github.com>
2022-12-08 12:29:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-    apiKey: "AIzaSyBIHWP80hIB7MfJlyyKHP7V_Yuv439fCrA",
-    authDomain: "whatsapp-web-b773f.firebaseapp.com",
-    projectId: "whatsapp-web-b773f",
-    storageBucket: "whatsapp-web-b773f.appspot.com",
-    messagingSenderId: "943200019074",
-    appId: "1:943200019074:web:6e58b2356dd2a8e19fca9b"
+  apiKey: "AIzaSyAqwl8x3qHRmmCBu7pXVtvAbWqk0hesDyU",
+  authDomain: "whats-app-web-clone-fc230.firebaseapp.com",
+  projectId: "whats-app-web-clone-fc230",
+  storageBucket: "whats-app-web-clone-fc230.appspot.com",
+  messagingSenderId: "122701087755",
+  appId: "1:122701087755:web:7f835b6b4d5aa43eb4f30e",
+  measurementId: "G-89Z0LZQX5L"
 };
 
 const app = initializeApp(firebaseConfig);
